214221cd61432c276aab12609f2e5789c9d7a967
Dominic Garcia <133527299+dominic-jpeg@users.noreply.github.com>
2025-02-19 17:36:37 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
+import unittest
 
+# Function to test
+def is_odd(n):
+    return n % 2 != 0
+
+class TestIsOdd(unittest.TestCase):
+
+    def test_odd_num(self):
+        self.assertTrue(is_odd(3))  # Testing an odd number
+
+    def test_even_num(self):
+        self.assertFalse(is_odd(4))  # Testing an even number
+
+    def test_zero(self):
+        self.assertFalse(is_odd(0))  # Testing zero as an edge case
+
+if __name__ == '__main__':
+    unittest.main()
